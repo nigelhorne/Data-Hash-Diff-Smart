46823a7b92eecc5a74d4a8466f5dd744c5d2c040
Nigel Horne <njh@bandsman.co.uk>
2026-04-02 20:53:31 -0400

    Release 0.02

--- lib/Data/Hash/Diff/Smart.pm
+++ lib/Data/Hash/Diff/Smart.pm
@@ -20,11 +20,11 @@ Data::Hash::Diff::Smart - Smart structural diff for Perl data structures
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
